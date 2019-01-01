1dfe86646e333572d0cac2a9428be564aadfbbbc
Zac Sweers <pandanomic@gmail.com>
2018-12-31 16:54:43 -0800

    ViewPumpActivityFactory -> Kotlin

---
+++ viewpump/src/main/java/io/github/inflationx/viewpump/-ViewPumpActivityFactory.kt
@@ -0,0 +1,31 @@
+@file:JvmName("-ViewPumpActivityFactory")
+package io.github.inflationx.viewpump
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+internal interface ViewPumpActivityFactory {
+
+  /**
+   * Used to Wrap the Activity onCreateView method.
+   *
+   * You implement this method like so in you base activity.
+   *
+   * ```
+   * public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+   *   return ViewPumpContextWrapper.get(getBaseContext()).onActivityCreateView(super.onCreateView(parent, name,context, attrs), attrs);
+   * }
+   * ```
+   *
+   * @param parent  parent view, can be null.
+   * @param view    result of `super.onCreateView(parent, name, context, attrs)`, this might be null, which is fine.
+   * @param name    Name of View we are trying to inflate
+   * @param context current context (normally the Activity's)
+   * @param attrs   see [android.view.LayoutInflater.Factory2.onCreateView]  @return the result from the activities `onCreateView()`
+   * @return The view passed in, or null if nothing was passed in.
+   * @see android.view.LayoutInflater.Factory2
+   */
+  fun onActivityCreateView(parent: View, view: View, name: String, context: Context,
+      attrs: AttributeSet): View?
+}
